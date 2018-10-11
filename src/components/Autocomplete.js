da1c2da242072eb773eabfed539d5af87e8bd28f
Patryk Karpiński <patryk.karpinski@softwarehut.com>
2018-10-11 12:59:13 +0200

    layout of autofill improved a bit

--- src/components/Autocomplete.js
+++ src/components/Autocomplete.js
@@ -113,7 +113,7 @@ class Autocomplete extends Component {
           onRequestClose={() => {
           Alert.alert('Modal has been closed.');
         }}>
-          <View style={styles.navbar}>
+          <View style={styles.backBtn}>
             <TouchableOpacity onPress={() => this.toggleModal()}>
               <Svg height="32" width="32">
                 <Path scale="1.5" d="M 10 5.3417969 C 9.744125 5.3417969 9.4879688 5.4412187 9.2929688 5.6367188 L 3.6367188 11.292969 C 3.2457187 11.683969 3.2457187 12.317031 3.6367188 12.707031 L 9.2929688 18.363281 C 9.6839688 18.754281 10.317031 18.754281 10.707031 18.363281 L 10.792969 18.277344 C 11.183969 17.886344 11.183969 17.253281 10.792969 16.863281 L 6.9296875 13 L 20 13 C 20.552 13 21 12.552 21 12 C 21 11.448 20.552 11 20 11 L 6.9296875 11 L 10.792969 7.1367188 C 11.183969 6.7457187 11.183969 6.1126563 10.792969 5.7226562 L 10.707031 5.6367188 C 10.511531 5.4412187 10.255875 5.3417969 10 5.3417969 z"
@@ -121,6 +121,8 @@ class Autocomplete extends Component {
                 />
               </Svg>
             </TouchableOpacity>
+          </View>
+          <View style={styles.navbar}>
             <View style={styles.inputContainer}>
               <TextInput
                 value={this.state.inputValue}
@@ -133,12 +135,16 @@ class Autocomplete extends Component {
               />
               {items ? <View style={styles.selectsContainer}>{items}</View> : null}
             </View>
-            <TouchableOpacity onPress={() => this.clearInput()}>
-              <Svg width="32" height="32">
-                <Line scale="1.5" fill="none" strokeWidth="2" stroke="#ff0033" x1="4" y1="4" x2="20" y2="20"/>
-                <Line scale="1.5" fill="none" strokeWidth="2" stroke="#ff0033" x1="20" y1="4" x2="4" y2="20"/>
-              </Svg>
-            </TouchableOpacity>
+
+            <View style={styles.delBtn}>
+              <TouchableOpacity onPress={() => this.clearInput()}>
+                <Svg width="32" height="32">
+                  <Line scale="1.5" fill="none" strokeWidth="2" stroke="#ff0033" x1="4" y1="4" x2="20" y2="20"/>
+                  <Line scale="1.5" fill="none" strokeWidth="2" stroke="#ff0033" x1="20" y1="4" x2="4" y2="20"/>
+                </Svg>
+              </TouchableOpacity>
+            </View>
+
           </View>
         </Modal>
         <TouchableOpacity onPress={() => this.toggleModal()}>
@@ -158,23 +164,38 @@ class Autocomplete extends Component {
 export default Autocomplete;
 
 const styles = StyleSheet.create({
+  backBtn: {
+    paddingTop: 5,
+    paddingLeft: 5,
+    backgroundColor: '#6c90bf',
+  },
   navbar: {
-    paddingTop: 24,
+    paddingTop: 12,
     paddingBottom: 24,
     flexDirection: 'row',
     justifyContent: 'space-around',
     alignItems: 'center',
-    backgroundColor: '#6c90bf'
+    backgroundColor: '#6c90bf',
   },
   inputContainer: {
     flexDirection: 'column',
     flex: 1,
+    paddingLeft: 10,
+    paddingRight: 10,
+    position: 'relative',
   },
   input: {
       borderWidth: 2,
       borderColor: "#555",
       backgroundColor: '#eee',
       minHeight: 52.6,
+  },
+  delBtn: {
+    paddingLeft: 5,
+    paddingRight: 5,
+    position: 'absolute',
+    right: 15,
+    top: 21,
   },
   selectsContainer: {
     flexDirection: 'column',
