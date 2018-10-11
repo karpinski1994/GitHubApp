72c1a16916d3e5c7292a05ccec2c9ea2fa4632f7
Patryk Karpiński <patryk.karpinski@softwarehut.com>
2018-10-11 12:05:32 +0200

    adjusting svg sizes

--- src/components/Autocomplete.js
+++ src/components/Autocomplete.js
@@ -1,8 +1,8 @@
 import React, {Component} from 'react';
-import {TextInput, Text, View, Modal, TouchableHighlight, TouchableOpacity, Alert, Dimensions } from 'react-native';
+import {TextInput, Text, View, Modal, TouchableOpacity, Alert, Dimensions } from 'react-native';
 import Svg,{
   Path,
-  G
+  Line
 } from 'react-native-svg';
 import {StyleSheet} from 'react-native';
 import { debounce } from 'lodash';
@@ -87,8 +87,6 @@ class Autocomplete extends Component {
 
   render() {
     let items = this.state.items;
-    console.log('inputValue: ', this.state.inputValue);
-    console.log('items: ', this.state.items);
     if(this.state.inputValue.length > this.props.minChars) {
       if (Array.isArray(items) && items.length > 0) {
         items = items.map(item => {
@@ -116,14 +114,13 @@ class Autocomplete extends Component {
           Alert.alert('Modal has been closed.');
         }}>
           <View style={styles.navbar}>
-            <TouchableHighlight onPress={() => this.toggleModal()}>
-              <Svg height="60" width="30">
-                <Path
-                    d="M10.273,5.009c0.444-0.444,1.143-0.444,1.587,0c0.429,0.429,0.429,1.143,0,1.571l-8.047,8.047h26.554  c0.619,0,1.127,0.492,1.127,1.111c0,0.619-0.508,1.127-1.127,1.127H3.813l8.047,8.032c0.429,0.444,0.429,1.159,0,1.587  c-0.444,0.444-1.143,0.444-1.587,0l-9.952-9.952c-0.429-0.429-0.429-1.143,0-1.571L10.273,5.009z"
-                    fill="black"
+            <TouchableOpacity onPress={() => this.toggleModal()}>
+              <Svg height="48" width="48">
+                <Path scale="2" d="M 10 5.3417969 C 9.744125 5.3417969 9.4879688 5.4412187 9.2929688 5.6367188 L 3.6367188 11.292969 C 3.2457187 11.683969 3.2457187 12.317031 3.6367188 12.707031 L 9.2929688 18.363281 C 9.6839688 18.754281 10.317031 18.754281 10.707031 18.363281 L 10.792969 18.277344 C 11.183969 17.886344 11.183969 17.253281 10.792969 16.863281 L 6.9296875 13 L 20 13 C 20.552 13 21 12.552 21 12 C 21 11.448 20.552 11 20 11 L 6.9296875 11 L 10.792969 7.1367188 C 11.183969 6.7457187 11.183969 6.1126563 10.792969 5.7226562 L 10.707031 5.6367188 C 10.511531 5.4412187 10.255875 5.3417969 10 5.3417969 z"
+                fill="black"
                 />
               </Svg>
-            </TouchableHighlight>
+            </TouchableOpacity>
             <View style={styles.inputContainer}>
               <TextInput
                 value={this.state.inputValue}
@@ -136,14 +133,12 @@ class Autocomplete extends Component {
               />
               {items ? <View style={styles.selectsContainer}>{items}</View> : null}
             </View>
-            <TouchableHighlight onPress={() => this.clearInput()}>
-              <Svg height="60" width="60">
-              <G>
-                <Path d="M26,0C11.664,0,0,11.663,0,26s11.664,26,26,26s26-11.663,26-26S40.336,0,26,0z M26,50C12.767,50,2,39.233,2,26   S12.767,2,26,2s24,10.767,24,24S39.233,50,26,50z" style="fill: rgb(216, 0, 39);"></Path>
-                <Path d="M35.707,16.293c-0.391-0.391-1.023-0.391-1.414,0L26,24.586l-8.293-8.293c-0.391-0.391-1.023-0.391-1.414,0   s-0.391,1.023,0,1.414L24.586,26l-8.293,8.293c-0.391,0.391-0.391,1.023,0,1.414C16.488,35.902,16.744,36,17,36   s0.512-0.098,0.707-0.293L26,27.414l8.293,8.293C34.488,35.902,34.744,36,35,36s0.512-0.098,0.707-0.293   c0.391-0.391,0.391-1.023,0-1.414L27.414,26l8.293-8.293C36.098,17.316,36.098,16.684,35.707,16.293z" style="fill: rgb(216, 0, 39);"></Path>
-              </G>
+            <TouchableOpacity onPress={() => this.clearInput()}>
+              <Svg  width="48" height="48">
+                <Line scale="2" fill="none" strokeWidth="2" stroke="black" x1="4" y1="4" x2="20" y2="20"/>
+                <Line scale="2" fill="none" strokeWidth="2" stroke="black" x1="20" y1="4" x2="4" y2="20"/>
               </Svg>
-            </TouchableHighlight>
+            </TouchableOpacity>
           </View>
         </Modal>
         <TouchableOpacity onPress={() => this.toggleModal()}>
@@ -166,20 +161,19 @@ const styles = StyleSheet.create({
   navbar: {
     marginTop: 22,
     flexDirection: 'row',
+    justifyContent: 'space-around',
   },
   inputContainer: {
     flexDirection: 'column',
     flex: 1,
-    height: 100,
   },
   input: {
       borderWidth: 2,
       borderColor: "#555",
       backgroundColor: '#eee',
-      marginTop:20,
       padding: 5,
+      minHeight: 48,
       flex: 1,
-      height: 30,
   },
   selectsContainer: {
     flexDirection: 'column',
